555ea8591332ef8fd4873dfe3d7f20f9bbeac411
Berke Saat <berkesaat@Berkes-MacBook-Pro.local>
2021-05-15 18:26:04 -0400

    encryption schemes added

---
+++ encrypt_data.py
@@ -0,0 +1,31 @@
+import csv
+from encryption import *
+
+
+#constants
+KEY_LEN = 10
+NUM_DATA = 500
+file_paths = ['data/passwords.csv', 'data/products.csv', 'data/usernames.csv']
+
+
+#Read CSV files.
+def get_data(enc_class = None):
+    data = []
+    for file in file_paths:
+        with open(file) as csv_file:
+            csv_reader = csv.reader(csv_file, delimiter=',')
+            line_count = 0
+            for row in csv_reader:
+                if line_count == 0:
+                    col_name = row[0]
+                    line_count += 1
+                elif line_count < NUM_DATA:
+                    raw_data = col_name + row[0]
+                    if enc_class == None:
+                        data.append(raw_data)
+                    else:
+                        data.append(enc_class.encrypt(raw_data))
+                    line_count += 1
+                else:
+                    break
+    return data
---
+++ encryption.py
@@ -0,0 +1,64 @@
+import numpy as np
+from string import ascii_uppercase, printable, ascii_lowercase
+import random
+
+#constants
+ALPH_LEN = 26
+PRINTS_LEN = 62
+
+def generate_conversion_dicts(is_alphabet = False):
+    
+    if is_alphabet:
+        mod = ALPH_LEN 
+        char_to_num = {letter: index for index, letter in enumerate(ascii_uppercase)}
+        num_to_char = {index: letter for index, letter in enumerate(ascii_uppercase)}
+
+    else:
+        mod = PRINTS_LEN
+        char_to_num = {letter: index for index, letter in enumerate(printable[:mod])}
+        num_to_char = {index: letter for index, letter in enumerate(printable[:mod])}
+        num_to_char[PRINTS_LEN] = '-'
+        num_to_char[PRINTS_LEN+1] = '_'
+        char_to_num['-'] = PRINTS_LEN
+        char_to_num['_'] = PRINTS_LEN+1
+        mod += 2
+
+    return mod, char_to_num, num_to_char
+
+    
+    
+class CaesarCipher:
+    
+    def __init__(self):
+
+        self.mod, self.char_to_num, self.num_to_char = generate_conversion_dicts()
+
+        self.shift = int(np.random.rand()*self.mod)
+
+    def encrypt(self, text):
+
+        result = [self.num_to_char[(self.char_to_num[char] + self.shift) % self.mod] for char in text]
+        return ''.join(result)
+            
+
+class OTP:
+
+    def __init__(self, length):
+
+        self.mod, self.char_to_num, self.num_to_char = generate_conversion_dicts()
+            
+        self.length = length
+        rand_nums = random.sample(range(self.mod), k=length)
+        rand_chars = [self.num_to_char[num] for num in rand_nums]
+        self.secret_key = ''.join(rand_chars)
+
+    def get_key_mod(self, key, length):
+
+        return (key * (int(length/len(key))+1))[:length]
+
+    def encrypt(self, text):
+    
+        key_mod = self.get_key_mod(self.secret_key, len(text))
+        enc_nums = [(self.char_to_num[text[i]] + self.char_to_num[key_mod[i]]) % self.mod for i in range(len(text))]
+        result = [self.num_to_char[num] for num in enc_nums]
+        return ''.join(result)
