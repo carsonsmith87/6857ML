16917f7c6dffb7968046cc21bcb046d5a830670a
Berke Saat <berkesaat@Berkes-MacBook-Pro.local>
2021-05-17 16:35:28 -0400

    no space char

--- encryption.py
+++ encryption.py
@@ -19,13 +19,13 @@ def generate_conversion_dicts(is_alphabet = False):
         num_to_char = {index: letter for index, letter in enumerate(printable[:mod])}
         num_to_char[PRINTS_LEN] = '-'
         num_to_char[PRINTS_LEN+1] = '_'
-        num_to_char[PRINTS_LEN+2] = ' '
-        num_to_char[PRINTS_LEN+3] = '!'
+        #num_to_char[PRINTS_LEN+2] = ' '
+        num_to_char[PRINTS_LEN+2] = '!'
         char_to_num['-'] = PRINTS_LEN
         char_to_num['_'] = PRINTS_LEN+1
-        char_to_num[' '] = PRINTS_LEN+2
-        char_to_num['!'] = PRINTS_LEN+3
-        mod += 4
+        #char_to_num[' '] = PRINTS_LEN+2
+        char_to_num['!'] = PRINTS_LEN+2
+        mod += 3
 
     return mod, char_to_num, num_to_char
 
